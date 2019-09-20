ea87490f218598fca1df0944c2e296ada8edef06
sanyinchen <sanyinchen@gmail.com>
2019-09-20 11:07:30 +0800

    [feature] update

--- java/src/main/java/leet_code/Fixed_point_1064.java
+++ java/src/main/java/leet_code/Fixed_point_1064.java
@@ -1,14 +1,23 @@
 package leet_code;
 
 public class Fixed_point_1064 {
+
     static class Solution {
+        int min = Integer.MAX_VALUE;
+
         private int fixedPoint(int[] A, int left, int right) {
             if (A == null || left > right) {
                 return -1;
             }
-            int mid = (left + right) / 2;
-            if (A[mid]==mid){
-                return mid;
+            int mid = left + (right - left) / 2;
+            if (A[mid] == mid) {
+                if (min > mid) {
+                    min = mid;
+                }
+                if (mid > 0 && A[mid - 1] == (mid - 1)) {
+                    return fixedPoint(A, left, mid-1);
+                }
+                // return mid;
             }
             if (A[mid] > mid) {
                 return fixedPoint(A, left, mid - 1);
@@ -18,7 +27,9 @@ public class Fixed_point_1064 {
         }
 
         public int fixedPoint(int[] A) {
-            return fixedPoint(A, 0, A.length - 1);
+            min = Integer.MAX_VALUE;
+            fixedPoint(A, 0, A.length - 1);
+            return min == Integer.MAX_VALUE ? -1 : min;
         }
     }
 
