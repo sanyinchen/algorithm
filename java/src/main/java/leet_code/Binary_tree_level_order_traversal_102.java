0722773d8a4a6fac6a3e6e95445cd0dc7ea376c0
sanyinchen <sanyinchen@gmail.com>
2019-10-19 13:19:14 +0800

    [feature] update

---
+++ java/src/main/java/leet_code/Binary_tree_level_order_traversal_102.java
@@ -0,0 +1,15 @@
+package leet_code;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import leet_code.common.TreeNode;
+
+public class Binary_tree_level_order_traversal_102 {
+    class Solution {
+        public List<List<Integer>> levelOrder(TreeNode root) {
+            List<List<Integer>> res = new ArrayList<>();
+            if (root)
+        }
+    }
+}
