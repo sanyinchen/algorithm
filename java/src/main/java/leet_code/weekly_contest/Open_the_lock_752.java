411f60b06f71af795ba15220e050878f03e9ca72
sanyinchen <sanyinchen@baidu.com>
2019-12-19 14:06:33 +0800

    【feature】 common update && add template

--- java/src/main/java/leet_code/weekly_contest/Open_the_lock_752.java
+++ java/src/main/java/leet_code/weekly_contest/Open_the_lock_752.java
@@ -1,5 +1,13 @@
 package leet_code.weekly_contest;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Set;
+
 /**
  * Created by sanyinchen on 19-9-22.
  *
@@ -10,55 +18,70 @@ package leet_code.weekly_contest;
 
 class Open_the_lock_752 {
     static class Solution {
-        private int childIndex = 0;
+        Set<String> cache = new HashSet<>();
 
-        private void openLockHelper(int[] childTargets, String target, int start) {
-
-//            if (n == 10) {
-//                System.out.println("=======>");
-//                return;
-//            }
-            StringBuilder temp = new StringBuilder();
-            // temp.append()
-            System.out.println(start + " " + childTargets[childIndex]);
-            for (int k = start; k < 4; k++) {
-                for (int i = childIndex; i < 9; i++) {
-                    childIndex++;
-                    openLockHelper(childTargets, target, k);
-                }
-                childIndex--;
-                System.out.println("=======>");
+        private String getLockStr(Integer[] locks) {
+            StringBuilder str = new StringBuilder();
+            for (int item : locks) {
+                str.append(item);
             }
-
+            return str.toString();
         }
 
-        private void Swap(int chs[], int i, int j) {
-            int temp;
-            temp = chs[i];
-            chs[i] = chs[j];
-            chs[j] = temp;
+        private List<Integer[]> nextLockList(Integer[] locks) {
+            List<Integer[]> tempList = new ArrayList<>();
+            for (int i = 0; i < locks.length; i++) {
+                Integer[] newLock = new Integer[locks.length];
+                System.arraycopy(locks, 0, newLock, 0, locks.length);
+                Integer[] newLock2 = new Integer[locks.length];
+                System.arraycopy(locks, 0, newLock2, 0, locks.length);
+                newLock[i] = (newLock[i] + 1 + 10) % 10;
+                newLock2[i] = (newLock2[i] - 1 + 10) % 10;
+                String lockStr = getLockStr(newLock);
+                if (!cache.contains(lockStr)) {
+                    tempList.add(newLock);
+                    cache.add(lockStr);
+                }
+
+                String lockStr2 = getLockStr(newLock2);
+                if (!cache.contains(lockStr2)) {
+                    tempList.add(newLock2);
+                    cache.add(lockStr2);
+                }
+            }
+            return tempList;
         }
 
         public int openLock(String[] deadends, String target) {
+            int number = 0;
+            cache.clear();
+            Set<String> deadendsSet = new HashSet<>(Arrays.asList(deadends));
 
-            int[] childTargets = new int[10];
-            for (int i = 0; i <= 9; i++) {
-                childTargets[i] = i;
+            Integer[] initlock = new Integer[]{0, 0, 0, 0};
+            LinkedList<Integer[]> queue = new LinkedList<>();
+            queue.add(initlock);
+            cache.add("0000");
+            while (!queue.isEmpty()) {
+                int size = queue.size();
+                while (size-- > 0) {
+                    Integer[] itemLock = queue.poll();
+                    String lockRank = getLockStr(itemLock);
+                    if (deadendsSet.contains(lockRank)) {
+                        continue;
+                    }
+                    if (target.equals(lockRank)) {
+                        return number;
+                    }
+                    queue.addAll(nextLockList(itemLock));
+                }
+                number++;
             }
-//            for (int k = 0; k < 4; k++) {
-//                StringBuilder stringBuilder = new StringBuilder();
-//                for (int i = k; i < 9; i++) {
-//                    stringBuilder.append(childTargets[i]);
-//                    // openLockHelper(childTargets, target, n + 1);
-//                }
-//
-//            }
-            openLockHelper(childTargets, target, 0);
-            return 0;
+
+            return -1;
         }
     }
 
     public static void main(String[] args) {
-        new Solution().openLock(new String[]{"8888"}, "0009");
+        System.out.println("==>" + new Solution().openLock(new String[]{"8888"}, "0009"));
     }
 }
