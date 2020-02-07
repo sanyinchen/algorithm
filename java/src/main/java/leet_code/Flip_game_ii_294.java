89f986c96dda039314bb602f3331e876c9848415
sanyinchen <1554194922@qq.com>
2020-02-07 12:40:59 +0800

    [feature] algorithm update

--- java/src/main/java/leet_code/Flip_game_ii_294.java
+++ java/src/main/java/leet_code/Flip_game_ii_294.java
@@ -1,30 +1,16 @@
 package leet_code;
 
-// todo fail
 public class Flip_game_ii_294 {
     static class Solution {
-        private boolean canWinHelper(char[] content, int times, int n, int start) {
-            System.out.println("start:" + start + " times:" + times);
-            if (start >= content.length) {
-                return times % 2 == 1;
-            }
-            boolean res = false;
-            for (int i = 0; i < n - 1; i++) {
-                int tempS = i;
-                int tempE = i + 1;
-                if (content[tempS] == '+' && content[tempE] == '+') {
-                    content[tempS] = '-';
-                    content[tempE] = '-';
-                    res |= canWinHelper(content, times + 1, n, tempE + 1);
-                    content[tempS] = '+';
-                    content[tempE] = '+';
+        public boolean canWin(String content) {
+            for (int i = 1; i < content.length(); i++) {
+                if (content.charAt(i - 1) == '+' && content.charAt(i) == '+') {
+                    if (!canWin(content.substring(0, i - 1) + "--" + content.substring(i + 1))) {
+                        return true;
+                    }
                 }
             }
-            return res;
-        }
-
-        public boolean canWin(String s) {
-            return canWinHelper(s.toCharArray(), 0, s.length(), 0);
+            return false;
         }
     }
 
