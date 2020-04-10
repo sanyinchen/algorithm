057cbe72dee8630bebff01715e90374e1d9cec4f
sanyinchen <1554194922@qq.com>
2020-04-10 23:57:47 +0800

    [feature] update

---
+++ cpp/leetcode/Add_digits_258.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    int addDigits(int num) {
+        if (num < 10) {
+            return num;
+        }
+        int temp = 0;
+        while (num > 0) {
+            temp += num % 10;
+            num /= 10;
+        }
+        return addDigits(temp);
+    }
+};
