ed96afc1fdae9fa1f46ea6eef199e3b9282d72fd
sanyinchen <sanyinchen@baidu.com>
2020-05-09 13:20:57 +0800

    [feature] common update

---
+++ js/leet_code/sqrtx_69.js
@@ -0,0 +1,12 @@
+/**
+ * Created by sanyinchen on 20-5-9.
+ *
+ * @author sanyinchen
+ * @version v0.1
+ * @since 20-5-9
+ */
+var mySqrt = function (x) {
+
+    return parseInt(Math.sqrt(x)+'');
+};
+
