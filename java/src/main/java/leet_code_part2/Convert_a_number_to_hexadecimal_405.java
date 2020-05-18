007f555ff03c9888482acb9811f9eb9bbdd69606
sanyinchen <sanyinchen@baidu.com>
2020-05-18 10:34:53 +0800

    [feature] algorithm update

---
+++ java/src/main/java/leet_code_part2/Convert_a_number_to_hexadecimal_405.java
@@ -0,0 +1,18 @@
+package leet_code_part2;
+
+/**
+ * Created by sanyinchen on 20-5-18.
+ *
+ * @author sanyinchen
+ * @version v0.1
+ * @since 20-5-18
+ */
+
+class Convert_a_number_to_hexadecimal_405 {
+    class Solution {
+        public String toHex(int num) {
+
+            return Integer.toHexString(num);
+        }
+    }
+}
