f537e2744da0e3f314dbaabfa934c0ada58d3b34
sanyinchen <sanyinchen@baidu.com>
2026-04-16 00:43:22 +0800

    Update maximum_total_damage_3186.rs

--- rust/src/maximum_total_damage_3186.rs
+++ rust/src/maximum_total_damage_3186.rs
@@ -1,30 +1,28 @@
 use crate::common::Solution;
 
 pub fn run_example() {
-    let nums = vec![1, 1, 3, 4];
+    // let nums = vec![1, 1, 3, 4];
+    let nums = vec![7, 1, 6, 3];
+
     let result = Solution::maximum_total_damage(nums);
     println!("{result:?}");
 }
 
+use std::collections::BTreeMap;
 impl Solution {
-    pub fn maximum_total_damage(power: Vec<i32>) -> i64 {
-        if power.is_empty() {
-            return 0;
+    pub fn maximum_total_damage(mut power: Vec<i32>) -> i64 {
+        let mut count = BTreeMap::new();
+        power.sort();
+        for value in power {
+            *count.entry(value).or_insert(0i64) += 1;
         }
 
-        let mut power = power;
-        power.sort_unstable();
-
-        let mut dp = Vec::new();
-
-        Self::get_damage(power, &mut dp)
-    }
-
-    fn get_damage(power: Vec<i32>, dp: &mut Vec<i32>) -> i64 {
-        for item in &power {
-            println!("{}", item)
+        let values: Vec<i32> = count.keys().copied().collect();
+        let mut dp = vec![0i64; values.len() + 1];
+        for i in 0..values.len() {
+            let compatible_count = values[..i].partition_point(|&x| x <= values[i] - 3);
+            dp[i + 1] = dp[i].max(dp[compatible_count] + values[i] as i64 * count[&values[i]]);
         }
-
-        return 0;
+        dp[dp.len() - 1]
     }
 }
